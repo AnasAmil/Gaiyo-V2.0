4e1f980f87c49e43d7973edef3a63d0d63175e1f
Yahya <ian.w.regs@gmail.com>
2021-03-24 14:08:58 +0100

    finished product component

--- code/frontend/src/app/app.module.ts
+++ code/frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
