fb85ffed575237273c6c4c75cd8465e3d9b2542f
Yahya <ian.w.regs@gmail.com>
2021-03-24 17:25:50 +0100

    finished footer

--- code/frontend/src/app/app.module.ts
+++ code/frontend/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductComponent } from './components/product/product.component';
 import { FeaturedComponent } from './components/featured/featured.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProductComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
